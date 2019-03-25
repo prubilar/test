3feeb70c8200417b8399600ec05fa0e0cebfb1c7
prubilar <prubilar@localhost.localdomain>
2019-03-25 14:28:22 -0300

    primer commit

---
+++ auto.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: prubilar
+ * Date: 13-12-18
+ * Time: 23:19
+ */
+
+class auto
+{
+
+    var $marca;
+    var $color;
+    var $puertas;
+
+    function __construct($marca,$color,$puertas)
+    {
+        $this->marca=$marca;
+        $this->color=$color;
+        $this->puertas=$puertas;
+    }
+
+    function acelerar(){
+        return "este auto esta acelerando";
+    }
+  function bocina(){
+        return "toca la bocina";
+  }
+}
+$auto1 = new auto('mazda','plateado',4);
+
+
+echo "Este auto es de marca $auto1->marca color $auto1->color y de $auto1->puertas puertas";
+echo $auto1->acelerar();
+echo $auto1->bocina();
---
+++ index.php
@@ -0,0 +1,52 @@
+<?php
+class Person {
+    protected $firstName; // public, protected <-> private
+    protected $lastName;
+    protected $nickname;
+    protected $changedNickname = 0;
+    protected $year;
+
+    public function __construct($firstName, $lastName,$year)
+    {
+        $this->firstName = $firstName;
+        $this->lastName = $lastName;
+        $this->year=$year;
+    }
+
+    public function getFirstName()
+    {
+        return $this->firstName;
+    }
+    public function getLastName()
+    {
+        return $this->lastName;
+    }
+    public function setNickname($nickname)
+    {
+        if ($this->changedNickname >= 2) {
+            throw new Exception(
+                "You can't change a nickname more than 2 times"
+            );
+        }
+        $this->nickname = $nickname;
+        $this->changedNickname++;
+    }
+
+
+
+
+    public function getNickname()
+    {
+        return $this->nickname;
+    }
+    public function getFullName()
+    {
+        return $this->firstName . ' ' . $this->lastName . ' '. $this->year;
+    }
+}
+$person1 = new Person('Duilio', 'Palacios', nacimiento
+);
+$person1->setNickname('Silence');
+$person1->setNickname('Sileence');
+exit($person1->getNickname());
+echo $person1->getFullName();
